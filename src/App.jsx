c4c885a2c7a615610c711cdce5a870fcc10d631a
Øyvind Straume Almli <oyvind.almli@gmail.com>
2020-10-16 11:58:52 +0200

    now with more modern react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import './TicTacToe.css';
+import Game from './Game.jsx';
+
+const App = () => (
+        <div className="App">
+            <Game/>
+        </div>
+    );
+
+export default App;
